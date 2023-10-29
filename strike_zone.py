cde8b2e4f094ee3a37debff41dc25c727b764962
Chris <nomail@mail.com>
2023-10-29 13:33:45 -0500

    Updates for homeplate detection and drawing correct strike zone

--- strike_zone.py
+++ strike_zone.py
@@ -23,9 +23,22 @@
 
 import sys
 import argparse
+import time
+
 
 from jetson_inference import poseNet, detectNet
 from jetson_utils import videoSource, videoOutput, Log, cudaDrawRect, cudaDrawLine
+
+
+# Red, Green, Blue, Alpha
+ZONE_BOX_COLOR = (157, 225, 157, 100)
+ZONE_LINE_WIDTH = 1
+ZONE_LINE_COLOR = (157, 225, 157, 255)
+
+MODELS_PATH = "training/models/homeplate"
+
+# how often, in seconds to run homeplate detection
+HOMEPLATE_DETECTION_PERIOD_SEC = 1
 
 
 def left_and_right_keypoint(pose, left_keypoint, right_keypoint):
@@ -67,7 +80,7 @@ def is_in_stance(pose):
         return False
 
 
-def get_zone(pose):
+def get_zone(pose, homeplate_left, homeplate_right):
     # return (left, top, right, bottom) tuple strike zone box
 
     # top of zone is midpoint between shoulder_y and hip_y
@@ -97,24 +110,20 @@ def get_zone(pose):
     # use lowest knee for bottom of zone
     bottom_zone = max(knees_y)
 
-    # TODO real home plate, for now use knees and some fixed numbers
-    right_zone = min(knees_x) - 60
-    left_zone = right_zone - 100
+    right_zone = homeplate_right
+    left_zone = homeplate_left
 
     return (left_zone, top_zone, right_zone, bottom_zone)
 
 def draw_zone(img, zone): 
     (left, top, right, bottom) = zone
 
-    BOX_COLOR = (255, 0, 0, 50)
-    cudaDrawRect(img, zone, BOX_COLOR)
+    cudaDrawRect(img, zone, ZONE_BOX_COLOR)
 
-    LINE_WIDTH = 1
-    LINE_COLOR = (255, 0, 0, 200)
-    cudaDrawLine(img, (left,top), (right,top), LINE_COLOR, LINE_WIDTH)
-    cudaDrawLine(img, (left,bottom), (right,bottom), LINE_COLOR, LINE_WIDTH)
-    cudaDrawLine(img, (left,top), (left,bottom), LINE_COLOR, LINE_WIDTH)
-    cudaDrawLine(img, (right,top), (right,bottom), LINE_COLOR, LINE_WIDTH)
+    cudaDrawLine(img, (left,top), (right,top), ZONE_LINE_COLOR, ZONE_LINE_WIDTH)
+    cudaDrawLine(img, (left,bottom), (right,bottom), ZONE_LINE_COLOR, ZONE_LINE_WIDTH)
+    cudaDrawLine(img, (left,top), (left,bottom), ZONE_LINE_COLOR, ZONE_LINE_WIDTH)
+    cudaDrawLine(img, (right,top), (right,bottom), ZONE_LINE_COLOR, ZONE_LINE_WIDTH)
 
 # parse the command line
 parser = argparse.ArgumentParser(description="Run pose estimation DNN on a video/image stream.",
@@ -125,13 +134,6 @@ parser.add_argument("input", type=str, default="",
                     nargs='?', help="URI of the input stream")
 parser.add_argument("output", type=str, default="",
                     nargs='?', help="URI of the output stream")
-parser.add_argument("--network", type=str, default="resnet18-body",
-                    help="pre-trained model to load (see below for options)")
-parser.add_argument("--overlay", type=str, default="links,keypoints",
-                    help="pose overlay flags (e.g. --overlay=links,keypoints)\nvalid combinations are:  'links', 'keypoints', 'boxes', 'none'")
-parser.add_argument("--threshold", type=float, default=0.15,
-                    help="minimum detection threshold to use")
-
 
 try:
     args = parser.parse_known_args()[0]
@@ -142,11 +144,7 @@ except:
 
 # load the pose estimation model
 pose_net = poseNet(network="resnet18-body", threshold=0.15)
-# net = detectNet(model="model/ssd-mobilenet.onnx", labels="model/labels.txt", 
-#                 input_blob="input_0", output_cvg="scores", output_bbox="boxes", 
-#                 threshold=args.threshold)
-MODELS = "training/models/homeplate"
-detect_net =  detectNet(model=f"{MODELS}/ssd-mobilenet.onnx", labels=f"{MODELS}/labels.txt", 
+detect_net =  detectNet(model=f"{MODELS_PATH}/ssd-mobilenet.onnx", labels=f"{MODELS_PATH}/labels.txt", 
                  input_blob="input_0", output_cvg="scores", output_bbox="boxes", 
                  threshold=0.3)
 
@@ -157,7 +155,11 @@ output = videoOutput(args.output, argv=sys.argv)
 print(f">>> sys.argv: {sys.argv}")
 print(f">>> input: {args.input}")
 print(f">>> output: {args.output}")
- 
+
+# time in seconds since last detection is run
+last_detection_sec = 0
+homeplate_left = 0
+homeplate_right = 0
 # process frames until EOS or the user exits
 while True:
     # capture the next image
@@ -166,48 +168,56 @@ while True:
     if img is None:  # timeout
         continue
 
+    current_time_sec = time.time()
+
+    # Detect Home Plate
+    if current_time_sec - last_detection_sec >= HOMEPLATE_DETECTION_PERIOD_SEC:
+        # detect objects in the image (with overlay)
+        detections = detect_net.Detect(img)
+
+        # print the detections
+        #print("detected {:d} objects in image".format(len(detections)))
+
+        for detection in detections:
+            #print(f"detection: {detection}")
+            if detection.ClassID == 1:
+                homeplate_left = detection.Left
+                homeplate_right = detection.Right
+                last_detection_sec = current_time_sec
+                break
 
 
-    # detect objects in the image (with overlay)
-    detections = detect_net.Detect(img)
+    # Detect and Draw strike zon
+    if homeplate_left > 0 and homeplate_right > 0:
+        # perform pose estimation (with overlay)
+        poses = pose_net.Process(img, overlay="links,keypoints")
 
-    # print the detections
-    print("detected {:d} objects in image".format(len(detections)))
+        # print the pose results
+        print("detected {:d} objects in image".format(len(poses)))
 
-    for detection in detections:
-        print(f"detection: {detection}")
+        for pose in poses:
+            print(pose)
+            print(pose.Keypoints)
+            print('Links', pose.Links)
 
-
-
-    # perform pose estimation (with overlay)
-    poses = pose_net.Process(img, overlay="links,keypoints")
-
-    # print the pose results
-    print("detected {:d} objects in image".format(len(poses)))
-
-    for pose in poses:
-        print(pose)
-        print(pose.Keypoints)
-        print('Links', pose.Links)
-
-        if (is_in_stance(pose)):
-            zone = get_zone(pose)
-            if zone:
-                draw_zone(img, zone)
+            if (is_in_stance(pose)):
+                zone = get_zone(pose, homeplate_left, homeplate_right)
+                if zone:
+                    draw_zone(img, zone)
+                else:
+                    print('No Strike Zone')
             else:
-                print('No Strike Zone')
-        else:
-            print('Not in stance!')
+                print('Not in stance!')
 
     # render the image
     output.Render(img)
 
     # update the title bar
-    output.SetStatus("{:s} | Network {:.0f} FPS".format(
-        args.network, pose_net.GetNetworkFPS()))
+    output.SetStatus("PoseNet Network {:.0f} FPS".format(
+        pose_net.GetNetworkFPS()))
 
     # print out performance info
-    pose_net.PrintProfilerTimes()
+    #pose_net.PrintProfilerTimes()
 
     # exit on input/output EOS
     if not input.IsStreaming() or not output.IsStreaming():
